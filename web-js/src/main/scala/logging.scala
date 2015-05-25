932313d6d799a560038548a1ed55ec36410a6919
Ian Murray <ian.c.murray@gmail.com>
2015-05-25 21:59:53 +0100

    [web-js] Use Log4JavaScript for the underlying logging framework

--- web-js/src/main/scala/logging.scala
+++ web-js/src/main/scala/logging.scala
@@ -1,28 +1,59 @@
 package uk.co.sprily
 package btf.webjs
 
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
 trait Logger {
-  val debug: String => Unit
-  val info: String => Unit
-  val warning: String => Unit
-  val error: String => Unit
+  def debug(msg: String): Unit
+  def info(msg: String): Unit
+  def warning(msg: String): Unit
+  def error(msg: String): Unit
 }
 
-/** Quite surprised I couldn't see anything built-in for this.
-  *
-  * Logging with this is as simple as it gets!
-  */
 object Logging {
 
-  def logger(ns: String): Logger = new Logger {
-    val debug   = log("debug") _
-    val info    = log("info") _
-    val warning = log("warning") _
-    val error   = log("error") _
+  private lazy val consoleAppender = new BrowserConsoleAppender()
 
-    private def log(level: String)(s: String) = {
-      println(s"[$level] $ns : $s")
-    }
+  def logger(ns: String): Logger = {
+    val underlying = Log4JavaScript.log4javascript.getLogger(ns)
+    underlying.addAppender(consoleAppender)
+    new LoggerImpl(underlying)
   }
 
+  private class LoggerImpl(underlying: Log4JSLogger) extends Logger {
+    override def debug(msg: String) = underlying.debug(msg)
+    override def info(msg: String) = underlying.info(msg)
+    override def warning(msg: String) = underlying.warn(msg)
+    override def error(msg: String) = underlying.error(msg)
+  }
+
+  object Log4JavaScript extends js.GlobalScope {
+    val log4javascript: Log4JavaScript = js.native
+  }
+
+  /** Interface to log4javascript **/
+  trait Log4JavaScript extends js.Object {
+    def getLogger(name: String): Log4JSLogger = js.native
+  }
+
+  @JSName("log4javascript.Logger")
+  trait Log4JSLogger extends js.Object {
+    def debug(msg: String): Unit = js.native
+    def info(msg: String): Unit = js.native
+    def warn(msg: String): Unit = js.native
+    def error(msg: String): Unit = js.native
+    def addAppender(a: Log4JSAppender): Unit = js.native
+  }
+
+
+  @JSName("log4javascript.Appender")
+  trait Log4JSAppender extends js.Object { }
+
+  @JSName("log4javascript.BrowserConsoleAppender")
+  class BrowserConsoleAppender extends Log4JSAppender
+
+  @JSName("log4javascript.AjaxAppender")
+  class AjaxAppender(url: String) extends Log4JSAppender { }
 }
+
